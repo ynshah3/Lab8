cb7b22585738b9f2e5022ba76af068f83a3be96d
ynshah3 <yashesperance3@gmail.com>
2021-02-26 15:50:13 -0500

    add lab writeup tests

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -1,9 +1,33 @@
 describe('Party Horn Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('http://127.0.0.1:5500/Part2-Cypress/index.html');
   });
 
   it('First Test', () => {
     expect(true).to.equal(true);
   });
+
+  it('Slider changes when volume input changes', () => {
+    cy.get('#volume-number').clear().type('75');
+    cy.get('#volume-slider')
+      .then(function change($el) {
+        expect($el).to.have.value(75);
+      })
+  })
+
+  it('Volume input changes when slider changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input');
+    cy.get('#volume-number')
+      .then(function change($el) {
+        expect($el).to.have.value(33);
+      })
+  })
+
+  it('Audio element changes when slider changes', () => {
+    cy.get('#volume-slider').invoke('val', 33).trigger('input');
+    cy.get('#horn-sound')
+      .then(function change($el) {
+        expect($el).to.have.prop('volume', 0.33);
+      })
+  })
 });
