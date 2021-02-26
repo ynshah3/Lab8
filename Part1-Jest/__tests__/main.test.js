a5b74634676c9ca51530eab9ec4d98e0d7dabfb9
ynshah3 <yashesperance3@gmail.com>
2021-02-26 15:06:19 -0500

    add jest tests for part 1

---
+++ Part1-Jest/__tests__/main.test.js
@@ -0,0 +1,32 @@
+const { expect } = require('@jest/globals');
+const formatVolumeIconPath = require('../assets/scripts/main');
+
+describe('first if condition', () => {
+  test('volume value greater than 66', () => {
+    expect(formatVolumeIconPath(70)).toContain('3');
+  });
+
+  test('volume value less than or equal to 66', () => {
+    expect(formatVolumeIconPath(50)).toMatch(new RegExp('./assets/media/icons/volume-level-[0,1,2].svg'))
+  });
+});
+
+describe('second if condition', () => {
+  test('volume value greater than 33', () => {
+    expect(formatVolumeIconPath(34)).toContain('2');
+  });
+
+  test('volume value less than or equal to 33', () => {
+    expect(formatVolumeIconPath(33)).toMatch(new RegExp('./assets/media/icons/volume-level-[0,1].svg'))
+  });
+});
+
+describe('third if condition', () => {
+  test('volume value greater than 0', () => {
+    expect(formatVolumeIconPath(10)).toContain('1');
+  });
+
+  test('volume value less than or equal to 0', () => {
+    expect(formatVolumeIconPath(0)).toContain('0');
+  });
+});
